3c2f50453b7c0861fb9de76ee6a29945a4b9b754
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-09 18:08:13 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", 
+  ],
+  darkMode:"class",
+  theme: {
+    extend: {
+      colors: {
+          primary: '#202225',
+          secondary: '#5865f2'
+      }
+    },
+  },
+  plugins: [],
+}
