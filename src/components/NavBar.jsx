30c6ba011cd675e831a0f3f879da86471b172794
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-11 18:21:22 +0200

    change boolean navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(true);
 
   const handleNav = () => {
     setNav(!nav);
