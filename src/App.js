afcf1abfd166e4391c986a2a4c9a9680ab060245
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-11 19:26:01 +0200

    apply services section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import { Analytics } from "./components/Analytics";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Services from "./components/Services";
 import SideBar from "./sideBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <SideBar/>
+      <SideBar />
       <Hero />
       <Analytics />
+      <Services />
     </div>
   );
 }
