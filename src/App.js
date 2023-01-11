d005289e0df7153ef2683bb69d5ed0291b24b66d
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-11 16:44:23 +0200

    add nvarbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import NavBar from './components/NavBar';
 import SideBar from './sideBar';
 
 function App() {
   return (
-    <div className="flex">
-      <SideBar/>
+    <div className="">
+      <NavBar/>
+      {/* <SideBar/> */}
     </div>
   );
 }
